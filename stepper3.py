03b84de0f7c7ded0b6bac7b3941829ee1316d2f9
nicol4sb <nicolasb@gmail.com>
2020-06-01 16:14:42 +0200

    Update approach - step towards Bresenham's alog

--- stepper3.py
+++ stepper3.py
@@ -3,16 +3,16 @@ import time, math
 
 GPIO.setmode(GPIO.BOARD)
 
-ControlPinMotor1 = [7, 11, 13, 15]
-ControlPinMotor2 = [8, 10, 12, 16]
+rightMotorGPIOPins = [7, 11, 13, 15]
+leftMotorGPIOPins = [10, 12, 16, 18]
 
-for pin in ControlPinMotor1:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, 0)
+# set all GPIOs to 0
+def setGPIOsAsOutputAndTo0():
+    for pin in leftMotorGPIOPins, rightMotorGPIOPins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, 0)
 
-for pin in ControlPinMotor2:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, 0)
+setGPIOsAsOutputAndTo0()
 
 halfSteppinglPhase = [
     [1, 0, 0, 0],
@@ -25,89 +25,51 @@ halfSteppinglPhase = [
     [1, 0, 0, 1]
 ]
 
-halfSteppinglPhaseAnti = [
-    [0, 0, 0, 1],
-    [0, 0, 1, 1],
-    [0, 0, 1, 0],
-    [0, 1, 1, 0],
-    [0, 1, 0, 0],
-    [1, 1, 0, 0],
-    [1, 0, 0, 0],
-    [1, 0, 0, 1]
-]
+# assume motors are in these positions to start with 
+# Surely not true, but with 4096 steps, acceptable approximation
+rightMotorCurrentStep = 0
+leftMotorCurrentStep = 0
 
-def turnMotor(stringLength, rightMotorIfTrueElseLeft):
+def turnMotorByHalfStepping(numberOfHalfSteps, pinsToBeActivated):
 
-    # calculate number of steps given 25mm perimeter of pulley = 2048 steps -> 4096 half steps
-    numberOfHalfSteps = 4096/25 * abs(stringLength)
-
-    # if motor selected is the right one
-    pinsToBeActivated = ControlPinMotor1
-
-    # clock-wise rotation? for right motor - else invert
-    stepping = halfSteppinglPhaseAnti if stringLength > 0 else halfSteppinglPhase
-
-    if not rightMotorIfTrueElseLeft:
-        pinsToBeActivated = ControlPinMotor2
+    currentHalfstep = 0
 
     # rotate
-    for _ in range(int(numberOfHalfSteps/4)):     # divide by 4 because we take 4 steps per loop -
-        for halfStep in range(8):
-            for pin in range(4):
-                GPIO.output(pinsToBeActivated[pin], stepping[halfStep][pin])
-            time.sleep(0.0015)
+    increment = 1 if numberOfHalfSteps > 0 else -1
+    for _ in range(abs(numberOfHalfSteps)):
+        for pin in range(4):
+            GPIO.output(pinsToBeActivated[pin], halfSteppinglPhase[currentHalfstep][pin])
+        currentHalfstep = (currentHalfstep + 8 + increment) % 8 # iterating over the 8 half steps
+        time.sleep(0.0007)
 
+turnMotorByHalfStepping(4096,leftMotorGPIOPins)
+turnMotorByHalfStepping(4096,rightMotorGPIOPins)
+turnMotorByHalfStepping(-4096,leftMotorGPIOPins)
+turnMotorByHalfStepping(-4096,rightMotorGPIOPins)
+setGPIOsAsOutputAndTo0()
 
 def move(x0, y0, x1,y1):
 
     # distance between the two motors
     d = 290 #mm
 
+    # calculate the distance of string to be rolled per motor
     # turn left motor by ?
-    distmg = math.sqrt(y1**2+(d+x1)**2) - math.sqrt(y0**2+(d+x0)**2)
-    print("dist moteur gauche", distmg)
-    turnMotor(distmg, False)
+    stringDistanceLeftMotor = math.sqrt(y1**2+(d+x1)**2) - math.sqrt(y0**2+(d+x0)**2)
+    print("distance left motor : ", stringDistanceLeftMotor)
 
     # turn right motor by ?
-    distmd = math.sqrt(y1**2+(d-x1)**2) - math.sqrt(y0**2+(d-x0)**2)
-    print("dist moteur droit", distmd)
-    turnMotor(- distmd, True)
+    stringDistanceRightMotor = math.sqrt(y1**2+(d-x1)**2) - math.sqrt(y0**2+(d-x0)**2)
+    print("distance right motor : ", stringDistanceRightMotor)
 
+    turnMotors(stringDistanceLeftMotor,stringDistanceRightMotor)
 
-# initial position of the pen : 
-# x0 = 0  --- on the bissector between the 2 motors
-# y0 = 290 --- 290mm down on the bisector of the 2 motors
-x0 = 0
-y0 = 290
-
-# draw a square - yay ! - size unknown as lines are not straight - but it's a square. 
-for _ in range(400):
-    x1 = x0
-    y1 = y0+0.1
-    move(x0,y0,x1,y1)
-    x0 = x1
-    y0 = y1
-
-for _ in range(400):
-    x1 = x0-0.1
-    y1 = y0
-    move(x0,y0,x1,y1)
-    x0 = x1
-    y0 = y1
-
-for _ in range(400):
-    x1 = x0
-    y1 = y0-0.1
-    move(x0,y0,x1,y1)
-    x0 = x1
-    y0 = y1
-
-for _ in range(400):
-    x1 = x0+0.1
-    y1 = y0
-    move(x0,y0,x1,y1)
-    x0 = x1
-    y0 = y1
+# apply the Bresenham's algo to have a somewhat straight line
+def turnMotors(stringDistanceLeftMotor, stringDistanceRightMotor):
+    
+    # implement Bresenham's algo here
+    turnMotorByHalfStepping(int(stringDistanceLeftMotor/25*4096), leftMotorGPIOPins)
+    turnMotorByHalfStepping(int(stringDistanceRightMotor/25*4096), rightMotorGPIOPins)
 
 
 GPIO.cleanup()
