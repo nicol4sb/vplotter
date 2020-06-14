059d1a8a6642f1d76977bdfe1cb54c6147c52172
nicol4sb <nicolasb@gmail.com>
2020-06-14 19:07:02 +0200

    refactor and implement Bresenham's algo

--- stepper.py
+++ stepper.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import RPi.GPIO as GPIO
 import time, math
 
@@ -25,44 +27,91 @@ halfSteppinglPhase = [
     [1, 0, 0, 1]
 ]
 
-
-
-# assume motors are in these positions to start with 
+# assume motors are in these positions (out of 8 half steps in a cycle - so possibly off by 4) 
 # Surely not true, but with 4096 steps, acceptable approximation
 rightMotorCurrentStep = 0
 leftMotorCurrentStep = 0
-currentHalfstep = 0 # this initialization is not too relevant - we will miss a few starting points possibly
 
 def turnMotorByHalfStepping(numberOfHalfSteps, pinsToBeActivated):
 
-    global currentHalfstep
+    # =======================================
+    #TODO learn Python and refactor that properly - likely by building a motor class and passing a motor object
+    global rightMotorCurrentStep
+    global leftMotorCurrentStep
+
+    if pinsToBeActivated == rightMotorGPIOPins:
+        currentStep = rightMotorCurrentStep
+        rightMotorCurrentStep += numberOfHalfSteps
+    else:
+        currentStep = leftMotorCurrentStep
+        leftMotorCurrentStep += numberOfHalfSteps
+    # =======================================
 
     # rotate
     increment = 1 if numberOfHalfSteps > 0 else -1
     for _ in range(abs(numberOfHalfSteps)):
 
-        currentHalfstep = (currentHalfstep + 8 + increment) % 8 # iterating over the 8 half steps
-
+        currentStep = (currentStep + 8 + increment) % 8
         for pin in range(4):
-            GPIO.output(pinsToBeActivated[pin], halfSteppinglPhase[currentHalfstep][pin])
-
+            GPIO.output(pinsToBeActivated[pin], halfSteppinglPhase[currentStep][pin])
+    
+        # 0.0007 is the lower limit for a halfstep - after that coils dont have time to establish the mag field - maybe?
         time.sleep(0.0007)
         
-        # reset to 0 post move / ok as everything is sequential
-        setGPIOsAsOutputAndTo0()
+    # reset to 0 post move / ok as everything is sequential
+    setGPIOsAsOutputAndTo0()
 
-# apply the Bresenham's algo to have a somewhat straight line
+# test :
+# turnMotorByHalfStepping(+1000,leftMotorGPIOPins)
+# turnMotorByHalfStepping(-100,rightMotorGPIOPins)
+
+pulleyPerimeter = 25 #mm
 def turnMotors(stringDistanceLeftMotor, stringDistanceRightMotor):
     
-    leftSteps = int(stringDistanceLeftMotor*4096/25)
-    rightSteps = int(stringDistanceRightMotor*4096/25)
+    leftSteps = int(stringDistanceLeftMotor*4096/pulleyPerimeter)
+    rightSteps = int(stringDistanceRightMotor*4096/pulleyPerimeter)
 
+    # get around painful edge cases
+    rightSteps = 1 if rightSteps == 0 else rightSteps
+    leftSteps = 1 if leftSteps == 0 else leftSteps
+
+    # print("# leftSteps ",leftSteps)
+    # print("# rightSteps ",rightSteps)
+ 
     # implement Bresenham's algo here
-    turnMotorByHalfStepping(leftSteps, leftMotorGPIOPins)
-    turnMotorByHalfStepping(rightSteps, rightMotorGPIOPins)
+    slope = abs(rightSteps/leftSteps)
+    roundedSlope = int(slope) 
 
-turnMotors(10,-20)
+    # print(" slope ",slope)
+    # print(" roundedSlope ",roundedSlope)
 
+    deviationPerLoop = abs(slope - roundedSlope)
+    currentDeviation = 0
+    
+
+    for _ in range(0, abs(leftSteps)):
+
+        # turn left by 1
+        turnMotorByHalfStepping(1 if leftSteps>0 else -1, leftMotorGPIOPins)
+
+        # turn right by slope
+        turnMotorByHalfStepping(roundedSlope if rightSteps>0 else - roundedSlope, rightMotorGPIOPins)
+        currentDeviation += deviationPerLoop
+
+        # adjust current deviation if over 1
+        if currentDeviation > 1:
+            turnMotorByHalfStepping(roundedSlope if rightSteps>0 else - roundedSlope, rightMotorGPIOPins)
+            currentDeviation -= 1 + deviationPerLoop
+    
+        # print("current deviation", currentDeviation)
+
+turnMotors(5, 5)
+
+#########################################################
+# distance between the two motors
+distanceBetweenMotors = 290 #mm
+
+# initial position
 x0 = 0
 y0 = 290
 
@@ -71,21 +120,21 @@ def move(x1,y1):
     # current pen position :
     global x0, y0
 
-    # distance between the two motors
-    d = 290 #mm
-
     # calculate the distance of string to be rolled per motor
     # turn left motor by ?
-    stringDistanceLeftMotor = math.sqrt(y1**2+(d+x1)**2) - math.sqrt(y0**2+(d+x0)**2)
+    stringDistanceLeftMotor = math.sqrt(y1**2+(distanceBetweenMotors+x1)**2) - math.sqrt(y0**2+(distanceBetweenMotors+x0)**2)
     print("distance left motor : ", stringDistanceLeftMotor)
 
     # turn right motor by ?
-    stringDistanceRightMotor = math.sqrt(y1**2+(d-x1)**2) - math.sqrt(y0**2+(d-x0)**2)
+    stringDistanceRightMotor = math.sqrt(y1**2+(distanceBetweenMotors-x1)**2) - math.sqrt(y0**2+(distanceBetweenMotors-x0)**2)
     print("distance right motor : ", stringDistanceRightMotor)
 
     turnMotors(stringDistanceLeftMotor,stringDistanceRightMotor)
     
     x0,y0 = x1,y1 # update current pen position
+#########################################################
 
+# move(50, 290)
+# move(-50, 290)
 
 GPIO.cleanup()
