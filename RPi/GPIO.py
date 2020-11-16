8e9237510ccb270368dd9e4258a648ec7bc65808
nicol4sb <nicolasb@gmail.com>
2020-11-16 21:21:00 +0100

    continued debugging

---
+++ RPi/GPIO.py
@@ -0,0 +1,19 @@
+BOARD = 1
+OUT = 1
+IN = 1
+
+def setmode(a):
+   # print(a)
+   return
+def setup(a, b):
+   # print(a)
+   return
+def output(a, b):
+   #print(a)
+   return
+def cleanup():
+   # print('a')
+   return
+def setwarnings(flag):
+   # print('False')
+   return
--- stepper.py
+++ stepper.py
@@ -18,13 +18,13 @@ setGPIOsAsOutputAndTo0()
 
 halfSteppinglPhase = [
     [1, 0, 0, 0],
-    [1, 1, 0, 0],
-    [0, 1, 0, 0],
-    [0, 1, 1, 0],    
-    [0, 0, 1, 0],
-    [0, 0, 1, 1],
+    [1, 0, 0, 1],
     [0, 0, 0, 1],
-    [1, 0, 0, 1]
+    [0, 0, 1, 1],    
+    [0, 0, 1, 0],
+    [0, 1, 1, 0],
+    [0, 1, 0, 0],
+    [1, 1, 0, 0]
 ]
 
 # assume motors are in these positions (out of 8 half steps in a cycle - so possibly off by 4) 
@@ -42,6 +42,8 @@ def turnMotorByHalfStepping(numberOfHalfSteps, pinsToBeActivated):
     global rightMotorCurrentStep
     global leftMotorCurrentStep
 
+    # print("right current step", rightMotorCurrentStep, "left current step", leftMotorCurrentStep)
+
     if pinsToBeActivated == rightMotorGPIOPins:
         current_step = rightMotorCurrentStep
         rightMotorCurrentStep += numberOfHalfSteps
@@ -52,30 +54,34 @@ def turnMotorByHalfStepping(numberOfHalfSteps, pinsToBeActivated):
     # =======================================
 
     # rotate
-    increment = numberOfHalfSteps/abs(numberOfHalfSteps)
+    increment = 1 if numberOfHalfSteps>0 else -1
     for _ in range(abs(numberOfHalfSteps)):
 
         # go through the steps so that a positive number of steps equals a string extension on the pulley
-        current_step = (current_step + 8 - increment) % 8
+        current_step = (current_step + 8 + increment) % 8
+
+        # print("Motor ", "right" if (pinsToBeActivated == rightMotorGPIOPins) else "left","current step ", current_step)
         
         # set pins so the appropriate step is activated
         for pin in range(4):
             GPIO.output(pinsToBeActivated[pin], halfSteppinglPhase[current_step][pin])
     
         # 0.0007 is the lower limit for a halfstep - after that coils dont have time to establish the mag field - maybe?
-        time.sleep(0.007)
+        time.sleep(0.0007)
         
 # test :
 # 4096 half steps = one full turn
-# turnMotorByHalfStepping(+4096,leftMotorGPIOPins)
-# turnMotorByHalfStepping(-4096,leftMotorGPIOPins)
-# turnMotorByHalfStepping(409,rightMotorGPIOPins)
-turnMotorByHalfStepping(100,leftMotorGPIOPins)
+# turnMotorByHalfStepping(+4,leftMotorGPIOPins)
+# turnMotorByHalfStepping(-3,leftMotorGPIOPins)
+# turnMotorByHalfStepping(+3,rightMotorGPIOPins)
+# turnMotorByHalfStepping(-4,rightMotorGPIOPins)
 
-for _ in range(100):
-    turnMotorByHalfStepping(1,leftMotorGPIOPins)
+# turnMotorByHalfStepping(100,rightMotorGPIOPins)
+# turnMotorByHalfStepping(100,leftMotorGPIOPins)
 
-'''
+
+
+
 # inner diameter where the thread is : 7.5mm :
 pulleyPerimeter = 23.56 #mm
 def turnMotors(stringDistanceLeftMotor, stringDistanceRightMotor):
@@ -101,21 +107,32 @@ def turnMotors(stringDistanceLeftMotor, stringDistanceRightMotor):
     current_deviation = 0
     for _ in range(0, abs(rightSteps)):
 
+        step = 1 if rightSteps>0 else -1
         # turn right motor by one step
-        turnMotorByHalfStepping(rightSteps/abs(rightSteps), rightMotorGPIOPins)
+        turnMotorByHalfStepping(step, rightMotorGPIOPins)
+        print("Right step ", step)
 
         # turn left by slope
         turnMotorByHalfStepping(rounded_slope, leftMotorGPIOPins)
+        print("Left step ", rounded_slope)
+        
         current_deviation += (slope - rounded_slope)
+        print("Deviation ", current_deviation)
 
         # adjust until deviation is < 1 step
         while current_deviation > 1:
             turnMotorByHalfStepping(1, rightMotorGPIOPins)
             current_deviation -= 1
+            print("Absorbing deviation - current ", current_deviation)
+
 
 # test : both motors should let go 10mm of string
-# turnMotors(0, +2.356)
-# turnMotors(0, -2.356)
+
+turnMotors(-10, -10)
+
+# turnMotors(10, -10)
+# turnMotors(-10, 10)
+
 
 #########################################################
 # distance between the two motors
@@ -149,7 +166,7 @@ def move(x1,y1):
 # move(40, 330)
 # reset to 0 post move / ok as everything is sequential
 
-'''
+
 setGPIOsAsOutputAndTo0()
 
 GPIO.cleanup()
